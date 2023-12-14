15df3e4fe0dd9452549c2198e13490f7c5312c73
FalcoN <falcon_gabriel@outlook.com>
2023-12-14 17:19:45 -0300

    Completando a função redirectToAuthCodeFlow

--- spotify-profile-demo/src/redirectToAuthCodeFlow.js
+++ spotify-profile-demo/src/redirectToAuthCodeFlow.js
@@ -1,7 +1,18 @@
 export default async function redirectToAuthCodeFlow(clientID) {
     const verifier = generateCodeVerifier(128);
     const challenge = await generateCodeChallenge(verifier);
-    console.log(challenge);
+
+    localStorage.setItem('verifier', verifier);
+    
+    const params = new URLSearchParams();
+    params.append('client_id', clientID);
+    params.append('response_type', 'code')
+    params.append('redirect_uri', 'http://localhost:5173/callback')
+    params.append('scope','user-read-private user-read-email')
+    params.append('code_challenge_method', 'S256')
+    params.append('code_challenge', challenge)
+    
+    document.location = `https://accounts.spotify.com/authorize?${params.toString()}`
 }
 
 function generateCodeVerifier(length) {
