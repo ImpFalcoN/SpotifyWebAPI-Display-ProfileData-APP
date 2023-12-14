dfd52fbccd191046623b69f08f075d30d531c00c
FalcoN <falcon_gabriel@outlook.com>
2023-12-14 17:33:28 -0300

    Adiciona Fluxo de verificação para execução da função redirectToAuthCodeFlow

--- spotify-profile-demo/src/script.js
+++ spotify-profile-demo/src/script.js
@@ -1,5 +1,10 @@
 import redirectToAuthCodeFlow from "./redirectToAuthCodeFlow";
 
 const clientId = '771315a53c91488399694c4bec1ae2eb';
+const params = new URLSearchParams(window.location.search);
+const code = params.get('code');
 
-redirectToAuthCodeFlow();
+if(!code) {
+    redirectToAuthCodeFlow(clientId)
+}else{
+}
