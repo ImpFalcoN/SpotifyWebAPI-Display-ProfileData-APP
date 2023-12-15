8f650b687a92471f0aa59bcb4db37c4c557c4f97
FalcoN <falcon_gabriel@outlook.com>
2023-12-15 13:33:01 -0300

    Resolvendo bug de requisição

--- spotify-profile-demo/src/getAcessToken.js
+++ spotify-profile-demo/src/getAcessToken.js
@@ -1,8 +1,7 @@
-export default async function getAcessToken(clientId, code) {
-    let verifier = localStorage.getItem('verifier')
+export default async function getAccessToken(clientId, code) {
+    const verifier = localStorage.getItem("verifier");
 
     const params = new URLSearchParams();
-
     params.append("client_id", clientId);
     params.append("grant_type", "authorization_code");
     params.append("code", code);
@@ -10,11 +9,11 @@ export default async function getAcessToken(clientId, code) {
     params.append("code_verifier", verifier);
 
     const result = await fetch("https://accounts.spotify.com/api/token", {
-        method: 'POST',
-        headers: { "content-Type": "application/x-www-form-urlencoded" },
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: params
-    })
+    });
 
-    const { acess_token } = await result.json();
-    return acess_token;
+    const { access_token } = await result.json();
+    return access_token;
 }
