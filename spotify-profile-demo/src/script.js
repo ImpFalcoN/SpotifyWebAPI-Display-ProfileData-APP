1906b8da9b5568aff1cdb3b3ce60232500204eef
FalcoN <falcon_gabriel@outlook.com>
2023-12-15 13:49:56 -0300

    Juntando as funções criadas

--- spotify-profile-demo/src/script.js
+++ spotify-profile-demo/src/script.js
@@ -1,3 +1,6 @@
+import fetchProfile from "./fetchProfile";
+import getAcessToken from "./getAcessToken";
+import populateUI from "./populateUI";
 import redirectToAuthCodeFlow from "./redirectToAuthCodeFlow";
 
 const clientId = '771315a53c91488399694c4bec1ae2eb';
@@ -7,4 +10,7 @@ const code = params.get('code');
 if(!code) {
     redirectToAuthCodeFlow(clientId)
 }else{
+    const acessToken = await getAcessToken(clientId, code);
+    const profile = await fetchProfile(acessToken);
+    populateUI(profile);
 }
